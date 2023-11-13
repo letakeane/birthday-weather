608a9c3ff6c0964f1b09f3cced64817c8e0b9889
Leta Keane <leta@hey.com>
2023-11-13 14:36:09 -0700

    Pivot to moon info app

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,9 @@
-function sayHi() {
-  console.log("Hi")
+function getWeather(location, date) {
+  return fetch(`http://api.weatherapi.com/v1/astronomy.json?key=${process.env.API_KEY}&q=${location}&dt=${date}`)
+  .then(response => response.json())
+  .catch(error => error);
 }
 
-export default sayHi;
+export {
+  getWeather
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import sayHi from './apiCalls.js';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { getWeather } from './apiCalls.js';
 
-console.log('Running')
-sayHi();
+dayjs.extend(customParseFormat);
+const dateString = dayjs().format('YYYY-MM-DD');
+const zip = document.querySelector('#zip');
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  if (zip.value) {
+    const zipcode = zip.value;
+    getWeather(zipcode, dateString).then(results => {
+      console.log(results)
+      zip.value = '';
+    })
+  } else {
+    alert('Please input a zip code.')
+  }
+}
